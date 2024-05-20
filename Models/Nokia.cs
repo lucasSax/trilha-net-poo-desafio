7ca92f28804423ad4c70df45474d36969e74fbeb
lucasSax <lucas.aut166@gmail.com>
2024-05-20 17:02:36 -0300

    Desafio concluído.

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
-namespace DesafioPOO.Models
+namespace DesafioPOO.Models;
+
+// TODO: Herdar da classe "Smartphone"
+public class Nokia : Smartphone
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
     }
+
+    public override void InstalarAplicativo(string nomeApp)
+    {
+        Console.WriteLine($"Instalando o aplicativo \"{nomeApp}\" no Nokia.");
+    }
+
+
+
+    // TODO: Sobrescrever o método "InstalarAplicativo"
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,85 @@
-﻿using DesafioPOO.Models;
+﻿
+using System;
+
+public class ItemMagico
+{
+    //TODO:  Crie a classe ItemMagico com os atributos nome, descrição e poder
+    public string Nome;
+    public string Descricao;
+    public int Poder;
+
+    public ItemMagico(string nome, string descricao, int poder)
+    {
+        Nome = nome;
+        Descricao = descricao;
+        Poder = poder;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Recebe a entrada do usuário
+
+        string nome = Console.ReadLine();
+
+
+        string descricao = Console.ReadLine();
+
+
+     int poder = Convert.ToInt32(Console.ReadLine());
+
+        // Cria o item mágico
+        ItemMagico item = new ItemMagico(nome, descricao, poder);
+
+        // Imprime o item criado
+        Console.WriteLine($"Item: {item.Nome}\nDescrição: {item.Descricao}\nPoder: {item.Poder}");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// using DesafioPOO.Models;
+
+
+
+// Console.WriteLine("Smatphone Nokia");
+// Smartphone nokia = new Nokia(numero: "548552", modelo: "modelo 1", imei: "2222222", memoria: 64);
+// nokia.Ligar();
+// nokia.InstalarAplicativo("Whatsapp");
+
+// Console.WriteLine("\n");
+
+// Console.WriteLine("Smatphone Iphone");
+// Smartphone iphone = new Iphone(numero: "984514", modelo: "modelo 2", imei: "0000", memoria: 128);
+// iphone.Ligar();
+// iphone.InstalarAplicativo("Telegram");
+
+// Console.WriteLine("\n");
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
